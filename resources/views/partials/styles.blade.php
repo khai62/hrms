f38f8bcf4a3d7211d1c8a166aa6eb40467b01d60
Khai <macbookpro@MacBooks-MacBook-Pro.local>
2025-11-23 11:05:50 +0700

    updatae

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -1,14 +1,13 @@
 <!-- Favicon -->
-<link rel="shortcut icon" type="image/x-icon" href="{{ Vite::asset('resources/assets/img/favicon.png') }}">
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png') }}">
 <link rel="stylesheet" href="{{ asset('js/plugins/bootstrap-datetimepicker/bootstrap-datetimepicker.min.css') }}">
-@vite([
-    'resources/assets/css/bootstrap.min.css',
-    'resources/assets/css/line-awesome.min.css',
-    'resources/assets/css/material.css',
-    'resources/assets/plugins/bootstrap-tagsinput/bootstrap-tagsinput.css',
-    'resources/assets/css/style.css',
-    'resources/css/app.scss',
-])
+
+<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/line-awesome.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/material.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap-tagsinput/bootstrap-tagsinput.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
 <!-- Vendor CSS -->
 @stack('vendor-styles')
 @yield('vendor-styles')
